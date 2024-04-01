d0071ba2f8147d916669bfb3ea01c7805f7794a3
Luis Ramon Diaz <diazluisramon.dev@gmail.com>
2024-04-01 17:56:18 -0400

    add font family and customize aside menu

--- src/app/components/side-menu/side-menu.component.ts
+++ src/app/components/side-menu/side-menu.component.ts
@@ -12,59 +12,59 @@ export class SideMenuComponent {
   menuOptionsList: AsideMenu[] = [
     {
       option: 'Conversaciones',
-      svgIcon: '@/assets/apps.svg',
+      svgIcon: '../../../assets/apps.svg',
     },
     {
       option: 'Item de las Ofertas',
-      svgIcon: '@/assets/apps.svg',
+      svgIcon: '../../../assets/apps.svg',
     },
     {
       option: 'Empleados',
-      svgIcon: '@/assets/apps.svg',
+      svgIcon: '../../../assets/apps.svg',
     },
     {
       option: 'Control y Seguridad',
-      svgIcon: '@/assets/apps.svg',
+      svgIcon: '../../../assets/apps.svg',
     },
     {
       option: 'Economía',
-      svgIcon: '@/assets/apps.svg',
+      svgIcon: '../../../assets/apps.svg',
     },
     {
       option: 'Comercial',
-      svgIcon: '@/assets/apps.svg',
+      svgIcon: '../../../assets/apps.svg',
     },
     {
       option: 'Mercado',
-      svgIcon: '@/assets/apps.svg',
+      svgIcon: '../../../assets/apps.svg',
     },
     {
       option: 'Operación',
-      svgIcon: '@/assets/apps.svg',
+      svgIcon: '../../../assets/apps.svg',
     },
     {
       option: 'Administración',
-      svgIcon: '@/assets/apps.svg',
+      svgIcon: '../../../assets/apps.svg',
     },
     {
       option: 'Otros',
-      svgIcon: '@/assets/apps.svg',
+      svgIcon: '../../../assets/apps.svg',
     },
     {
       option: 'Jurídico',
-      svgIcon: '@/assets/apps.svg',
+      svgIcon: '../../../assets/apps.svg',
     },
     {
       option: 'Reportes',
-      svgIcon: '@/assets/apps.svg',
+      svgIcon: '../../../assets/apps.svg',
     },
     {
       option: 'Aplicaciones',
-      svgIcon: '@/assets/apps.svg',
+      svgIcon: '../../../assets/apps.svg',
     },
     {
       option: 'Ajustes',
-      svgIcon: '@/assets/apps.svg',
+      svgIcon: '../../../assets/apps.svg',
     },
   ];
 }
