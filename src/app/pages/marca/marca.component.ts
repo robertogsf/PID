99d008b361fc373f657b9497b78ff2b7980ad8ce
Luis Ramon Diaz <diazluisramon.dev@gmail.com>
2024-04-02 17:31:25 -0400

    logic marca

--- src/app/pages/marca/marca.component.ts
+++ src/app/pages/marca/marca.component.ts
@@ -1,12 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  NgForm,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-marca',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './marca.component.html',
-  styleUrl: './marca.component.css'
+  styleUrl: './marca.component.css',
 })
 export class MarcaComponent {
+  form: FormGroup;
+  @ViewChild('denomInput') denomInput!: ElementRef;
 
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      denom: ['', Validators.required],
+      abr: ['', Validators.required],
+    });
+  }
+
+  public resetForm() {
+    this.form.reset();
+    this.denomInput.nativeElement.focus();
+  }
+
+  public onSubmit() {
+    if (this.form.valid) {
+      console.log(this.form.value);
+      this.form.reset();
+    } else {
+      window.alert('Formulario inválido');
+    }
+  }
 }
