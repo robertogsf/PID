dc37f9caf79ae15ce5139e741c33b7e223a77a8a
Luis Ramon Diaz <diazluisramon.dev@gmail.com>
2024-04-02 14:04:02 -0400

    fix layout comp

--- src/app/components/layout/layout.component.ts
+++ src/app/components/layout/layout.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from '../header/header.component';
 import { SideMenuComponent } from '../side-menu/side-menu.component';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-layout',
